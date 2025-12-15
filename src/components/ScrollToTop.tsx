e2557af1dd5db74b8909647c7b721cd36c9d826c
visacci <visacciallanz14@gmail.com>
2025-12-15 09:23:48 +0300

    Always at the top

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,21 @@
+// src/components/ScrollToTop.tsx
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top instantly
+    window.scrollTo(0, 0);
+
+    // Optional: For smooth scrolling
+    // window.scrollTo({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
+  }, [pathname]);
+
+  return null;
+};
